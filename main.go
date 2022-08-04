348f46c38bdb00ee7ec2ae2f122049155d687213
Morten Jørgensen <mjarg19@student.aau.dk>
2022-08-04 13:13:33 +0200

    chat tcp service, telnet bad though

--- main.go
+++ main.go
@@ -1,7 +1,30 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net"
+)
 
 func main() {
-	fmt.Println("Hello, world.")
+	s := newServer()
+	go s.run()
+
+	listener, err := net.Listen("tcp", ":8888")
+
+	if err != nil {
+		log.Fatalf("Could not start server: %s", err.Error())
+	}
+
+	defer listener.Close()
+	log.Printf("Started server on :8888")
+
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			log.Printf("unable to accept connection: %s", err.Error())
+			continue
+		}
+
+		go s.newClient(conn)
+	}
 }
